e89f14dc617f0be9d4e5d5de1ea4f3be45d54b38
Chiccoyhul <yulcisnerosmolina@gmail.com>
2023-04-19 09:43:57 -0500

    La primera plantilla de chicco

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="bg-dark text-white text-center py-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <p>Atrapando Poquemones - Todos los derechos reservados Atrapalos ya!!!!!</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
---
+++ index.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>La Pagina de Chicco</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  
+<body>
+
+<?php include('menu.php'); ?>
+
+
+<div class="container my=3">
+    
+     <h1>Chicco Cisneros</h1> <br>
+    
+     <div class="row">
+
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/agua.jpg" alt="Imagen 1" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Bolbasor</h5>
+             <p class="card-text">Las características son los seis parámetros principales que determinan cómo se desempeñará un Pokémon en combate. Se trata de los PS, el Ataque, la Defensa, el Ataque Especial, la Defensa Especial y la Velocidad.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/panda.png" alt="Imagen 2" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Megasor</h5>
+             <p class="card-text">Las características son los seis parámetros principales que determinan cómo se desempeñará un Pokémon en combate. Se trata de los PS, el Ataque, la Defensa, el Ataque Especial, la Defensa Especial y la Velocidad.!</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+      
+      
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/MAR.jpg" alt="Imagen 3" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Martillo</h5>
+             <p class="card-text">Las características son los seis parámetros principales que determinan cómo se desempeñará un Pokémon en combate. Se trata de los PS, el Ataque, la Defensa, el Ataque Especial, la Defensa Especial y la Velocidad.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+        </div>
+        
+       
+     
+     
+    
+      <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/MEGAS.jpg" alt="Imagen 4" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Zapatilla</h5>
+             <p class="card-text">Las características son los seis parámetros principales que determinan cómo se desempeñará un Pokémon en combate. Se trata de los PS, el Ataque, la Defensa, el Ataque Especial, la Defensa Especial y la Velocidad.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+      
+       
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/TORTI.jpg" alt="Imagen 5" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Target</h5>
+             <p class="card-text">Las características son los seis parámetros principales que determinan cómo se desempeñará un Pokémon en combate. Se trata de los PS, el Ataque, la Defensa, el Ataque Especial, la Defensa Especial y la Velocidad..</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+       
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/TORTI.jpg" alt="Imagen 6" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Tortuga</h5>
+             <p class="card-text">Las características son los seis parámetros principales que determinan cómo se desempeñará un Pokémon en combate. Se trata de los PS, el Ataque, la Defensa, el Ataque Especial, la Defensa Especial y la Velocidad.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+       </div> 
+       </div><br>
+      
+
+       <?php include('footer.php'); ?>
+ 
+       
+  
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+
+
+
+</body>
+
